de4e0439154f4c2eb8b25deede61bc5e2c16442b
zhaofeng-shu33 <616545598@qq.com>
2019-09-24 21:21:52 +0800

    port old code to test dir

--- test/test_bitset_hash.c
+++ test/test_bitset_hash.c
@@ -10,15 +10,15 @@ int main(int argc, char *argv[]) {
 	for (guint32 ii = 1; ii < size; ii++) {
 		bitset_set(aa, ii);
 		hh = bitset_hash(aa);
-		g_print("%d: %x\n", ii, hh);
+		// g_print("%d: %x\n", ii, hh);
 		for (guint32 jj = ii+1; jj < size; jj++) {
 			bitset_set(aa, jj);
 			hh = bitset_hash(aa);
-			g_print("%d, %d: %x\n", ii, jj, hh);
+			// g_print("%d, %d: %x\n", ii, jj, hh);
 			for (guint32 kk = jj+1; kk < size; kk++) {
 				bitset_set(aa, kk);
 				hh = bitset_hash(aa);
-				g_print("%d, %d, %d: %x\n", ii, jj, kk, hh);
+				// g_print("%d, %d, %d: %x\n", ii, jj, kk, hh);
 				bitset_clear(aa, kk);
 			}
 			bitset_clear(aa, jj);
